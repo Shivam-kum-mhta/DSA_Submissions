97be2287a6a1e54c1ea58cfab8192fd8a5a4a8cd
Shivam-Kumar-A <155562554+Shivam-kum-mhta@users.noreply.github.com>
2024-09-09 22:46:18 +0530

    Time: 12 ms (80.03%), Space: 22 MB (27.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int i,j;
-        j=0;
-        while(nums[j]!=0){
-            if(j>=nums.size()-1) return;
-            j++;
-        }
-        i=j;
-        j++;
+        i=j=0;
+        // while(nums[j]!=0){          //if j>nums.size()-1 then it can have undefined character , showing n=different behaviour
+        //     if(j>=nums.size()-1) return;
+        //     j++;
+        // }
+        // i=j;
+        // j++;
         for(;j<nums.size();j++)
         // while(nums[j]!=0){
             if(nums[j]!=0){
