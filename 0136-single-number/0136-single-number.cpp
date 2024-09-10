5b094dcb735b1ff66fcf413b6f2bf099c754fc75
Shivam-Kumar-A <155562554+Shivam-kum-mhta@users.noreply.github.com>
2024-09-10 18:10:02 +0530

    Time: 32 ms (9.13%), Space: 23.4 MB (18.14%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        set <int> s;
+        for(auto i: nums)
+        s.insert(i);
+    int sum=0;
+        for(auto i:nums)
+        sum+=i;
+
+        int setsum=0;
+        for(auto i:s)
+        setsum+=i;
+        setsum*=2;
+
+        return setsum-sum;
+    }
+};
