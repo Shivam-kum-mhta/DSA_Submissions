12372ba6fbf9fda27e6609c14dd1ab7abe074507
Shivam-Kumar-A <155562554+Shivam-kum-mhta@users.noreply.github.com>
2024-09-10 18:19:58 +0530

    Time: 13 ms (64.91%), Space: 19.6 MB (30.2%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        set <int> s;
-        for(auto i: nums)
-        s.insert(i);
-    int sum=0;
-        for(auto i:nums)
-        sum+=i;
+    //     set <int> s;
+    //     for(auto i: nums)
+    //     s.insert(i);
+    // int sum=0;
+    //     for(auto i:nums)
+    //     sum+=i;
 
-        int setsum=0;
-        for(auto i:s)
-        setsum+=i;
-        setsum*=2;
+    //     int setsum=0;
+    //     for(auto i:s)
+    //     setsum+=i;
+    //     setsum*=2;
 
-        return setsum-sum;
+    //     return setsum-sum;
+    int result=0;
+    for(auto i:nums){
+        result^=i;
+    }
+    return result;
     }
 };
