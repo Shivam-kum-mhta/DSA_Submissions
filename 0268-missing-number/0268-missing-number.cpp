b34409eafbb0a04577ffc0eb96cc581a10c10b5c
Shivam-Kumar-A <155562554+Shivam-kum-mhta@users.noreply.github.com>
2024-09-10 17:41:51 +0530

    Time: 7 ms (96.51%), Space: 20.7 MB (78.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int N=nums.size();
-        vector <int> hash(N+1,0);
-        for (int i=0;i<nums.size();i++)
-        hash[nums[i]]=hash[nums[i]]+1;
+     int missingNumber(vector<int>& nums) {
+    //     int N=nums.size();
+    //     vector <int> hash(N+1,0);
+    //     for (int i=0;i<nums.size();i++)
+    //     hash[nums[i]]=hash[nums[i]]+1;
 
-        for(int i=0;i<=nums.size();i++)
-        {if(hash[i]==0)
-        return i;}
+    //     for(int i=0;i<=nums.size();i++)
+    //     {if(hash[i]==0)
+    //     return i;}
     
-    return -1;
+    // return -1;
+    int s1=0;
+    int s2=(nums.size()*(nums.size()+1))/2;
+    for(int i=0;i<nums.size();i++)
+        s1+=nums[i];
+
+   return s2-s1;
     }
 };
