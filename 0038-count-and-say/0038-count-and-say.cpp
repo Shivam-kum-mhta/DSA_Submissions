9615f052aac6a55965661af0a154dcf6d0167310
Shivam-Kumar-A <155562554+Shivam-kum-mhta@users.noreply.github.com>
2024-09-26 01:49:00 +0530

    Time: 36 ms (5.15%), Space: 124.9 MB (6.56%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string findfreq(string s){
+        int count=1;
+        string s2="";
+        for(int i=1; i<s.size();i++){
+                if(s[i]==s[i-1]) {
+                    count++;
+                }
+                else{
+                    s2=s2+to_string(count)+s[i-1];
+                    count=1;
+                }
+            }
+         s2=s2+to_string(count)+s[s.size()-1];
+        return s2;
+    }
+    string countAndSay(int n) {
+      
+        if(n==1) return "1";
+          string s="1";
+        for(int i=1; i<n;i++){
+            s=findfreq(s);
+        }
+        return s;
+    }
+};
