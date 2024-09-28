181d99f9f6b2410e1657440729bcac49083bdd8d
Shivam-Kumar-A <155562554+Shivam-kum-mhta@users.noreply.github.com>
2024-09-29 03:27:25 +0530

    Time: 18 ms (53.3%), Space: 28.3 MB (6.62%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        int count=1;
-        int ans=nums[0];
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i]!=ans)
-            count--;
-            else count++;
-            if(count<0)
-            {ans=nums[i];
-            count=0;}
-
+        unordered_map<int, int> map;
+        int max=nums[0];
+        for(int i=0;i<nums.size();i++)
+        {
+            map[nums[i]]++;
         }
-        return ans;
+          for(auto i: map)
+        {
+            if(i.second>nums.size()/2)
+            {
+                return i.first;
+            }
+        }
+       return -1;
     }
 };
